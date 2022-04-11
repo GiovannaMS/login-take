c5f538aa966be7a51ac88095de5ab5e9327b9d13
Giovanna Monteiro <giovannams3999@gmail.com>
2022-04-11 17:31:54 -0300

    pequenos ajustes e comentarios

--- js/showPass.js
+++ js/showPass.js
@@ -1,11 +1,12 @@
+//botão de mostrar a senha
 function showPass() {
     var x = document.getElementById("password");
     var y = document.getElementById("eye");
     if (x.type === "text") {
-      x.type = "password";
-      y.style.fill="rgb(133, 131, 131)";
+      x.type = "password"; //senha escondida
+      y.style.fill="rgb(133, 131, 131)"; //botão fica cinza
     } else {
-      x.type = "text";
-      y.style.fill="#ffb200";
+      x.type = "text"; //senha a mostra
+      y.style.fill="#0474e4"; //botão fica amarelo
     }
   } 
